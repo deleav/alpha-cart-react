862e0559713eec81d0fdc6748bf24fda7931ea15
deleav <a42006310@gmail.com>
2022-09-28 00:25:36 +0800

    feat: add Step

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import styles from "./app.module.css";
-import PaymentInfo from "./components/checkoutStep/PaymentInfo";
-import ShippingInfo from "./components/checkoutStep/ShippingInfo";
-import ShippingMethod from "./components/checkoutStep/ShippingMethod";
+import styles from './app.module.css';
+import PaymentInfo from './components/checkoutStep/PaymentInfo';
+import ShippingInfo from './components/checkoutStep/ShippingInfo';
+import ShippingMethod from './components/checkoutStep/ShippingMethod';
+import StepContainer from './components/step/StepContainer';
 
 function App() {
   return (
     <div className={styles.app}>
-      <ShippingInfo />
+      <StepContainer />
+      <ShippingInfo onFormChange={() => {}} />
       <ShippingMethod />
       <PaymentInfo />
     </div>
