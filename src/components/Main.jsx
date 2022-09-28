0e745629c96f06506ac15597f015ef33e5740d23
deleav <a42006310@gmail.com>
2022-09-28 23:11:48 +0800

    feat: add Main

---
+++ src/components/Main.jsx
@@ -0,0 +1,66 @@
+import Cart from './cart/Cart';
+import PaymentInfo from './checkoutStep/PaymentInfo';
+import ShippingInfo from './checkoutStep/ShippingInfo';
+import ShippingMethod from './checkoutStep/ShippingMethod';
+import Footer from './footer/Footer';
+import styles from './main.module.css';
+import StepContainer from './step/StepContainer';
+
+export default function Main() {
+  const step = 1;
+
+  const handleFormChange = (name, value) => {
+    console.log('handleFormChange');
+  };
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.titleArea}>結帳</div>
+      <div className={styles.stepArea}>
+        <StepContainer currentStep={step} />
+      </div>
+      <div className={styles.routerArea}>
+        {(() => {
+          switch (step) {
+            case 2:
+              return (
+                <ShippingMethod
+                  shippingFee={0}
+                  onFormChange={handleFormChange}
+                />
+              );
+            case 3:
+              return (
+                <PaymentInfo
+                  ccname=""
+                  cardnumber=""
+                  expdate=""
+                  cvv=""
+                  onFormChange={handleFormChange}
+                />
+              );
+            case 1:
+            default:
+              return (
+                <ShippingInfo
+                  salutation=""
+                  username=""
+                  phone=""
+                  email=""
+                  city=""
+                  addr=""
+                  onFormChange={handleFormChange}
+                />
+              );
+          }
+        })()}
+      </div>
+      <div className={styles.footerArea}>
+        <Footer step={step} onSubmit={() => {}} onStepChange={() => {}} />
+      </div>
+      <div className={styles.cartArea}>
+        <Cart shippingFee={0} onChange={() => {}} />
+      </div>
+    </div>
+  );
+}
